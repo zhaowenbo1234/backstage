108eb1a3494d170085b12557c871b5a5ff4d07f6
济世魔王 <308539393@qq.com>
2018-12-04 21:51:17 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+简单后台管理系统 
+1 技术
+    1 springboot 2.1.1
+    2 mysql 5.6
+    3 th.mapper
+    4 thymeleaf
+    
+2 功能
+    1 简单用户权限 （先做这个，其余后续补充）
---
+++ src/main/java/com/zhaowb/boot/backstage/BackstageApplication.java
@@ -0,0 +1,12 @@
+package com.zhaowb.boot.backstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackstageApplication.class, args);
+    }
+}
