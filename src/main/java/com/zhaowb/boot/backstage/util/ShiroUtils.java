d439c0af1a1278d28494ed747e451194309faad6
zwb <308539393@qq.com>
2018-12-22 20:35:27 +0800

    shiro 工具类

---
+++ src/main/java/com/zhaowb/boot/backstage/util/ShiroUtils.java
@@ -0,0 +1,64 @@
+package com.zhaowb.boot.backstage.util;
+
+import com.zhaowb.boot.backstage.entity.User;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.BeanUtils;
+
+/**
+ * @Description shiro 工具类
+ * @auther zwb
+ * @create 2018-12-22 19:41
+ */
+public class ShiroUtils {
+
+
+    /**
+     * 获取shiro subject
+     * @return
+     */
+    public static Subject getSubject() {
+        return SecurityUtils.getSubject();
+    }
+
+    /**
+     * 获取  shiro session
+     * @return
+     */
+    public static Session getSession() {
+        return SecurityUtils.getSubject().getSession();
+    }
+
+    /**
+     * 退出
+     */
+    public static void logout() {
+        getSubject().logout();
+    }
+
+    public static User getSysUser() {
+        User user = null;
+        Object obj = getSubject().getPrincipal();
+        if (obj != null) {
+            user = new User();
+            BeanUtils.copyProperties(obj,user);
+        }
+        return user;
+    }
+
+
+    public static void setSysUser(User user)
+    {
+        Subject subject = getSubject();
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        String realmName = principalCollection.getRealmNames().iterator().next();
+        PrincipalCollection newPrincipalCollection = new SimplePrincipalCollection(user, realmName);
+        // 重新加载Principal
+        subject.runAs(newPrincipalCollection);
+    }
+
+}
