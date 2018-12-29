622fabbe5744b6b8b94b5694b8db594e182c2b72
zwb <308539393@qq.com>
2018-12-29 21:32:07 +0800

    修正注解错误

--- src/main/java/com/zhaowb/boot/backstage/controller/UserController.java
+++ src/main/java/com/zhaowb/boot/backstage/controller/UserController.java
@@ -3,9 +3,10 @@ package com.zhaowb.boot.backstage.controller;
 import com.zhaowb.boot.backstage.entity.User;
 import com.zhaowb.boot.backstage.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -15,25 +16,33 @@ import java.util.List;
  * @author zhaowb [308539393@qq.com]
  * @date 2018/12/6 17:48
  */
-@RestController
+@Controller
 @RequestMapping("user")
 public class UserController {
+
+    private final String PREFIX = "/user";
 
     @Autowired
     private IUserService userService;
 
+    @GetMapping
+    public String users() {
+        return PREFIX + "/user";
+    }
+
     @RequestMapping("/findUserByUserName")
-    public User findUserByUserName(String username){
+    public User findUserByUserName(String username) {
         return userService.findUserByUserName(username);
     }
 
     /**
      * 查询所有未删除用户
+     *
      * @return List<User>
      */
     @RequestMapping("/selectAllUsers")
     @ResponseBody
-    public List<User> selectAllUsers(){
+    public List<User> selectAllUsers() {
         return userService.selectAllUsers();
     }
 }
